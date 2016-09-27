6a8dcbe0d696cdf2844cb611f731db567c90951e
Asmir Mustafic <goetas@gmail.com>
2016-09-27 17:55:15 +0200

    ignore time when deciding mirror policy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ SOURCEFOLDER='output_prod'
 lftp -c "
 open $FTP_HOST
 user $FTP_USER $FTP_PWD
-mirror --reverse --delete --verbose $SOURCEFOLDER $TARGETFOLDER
+mirror --reverse --ignore-time --delete --verbose $SOURCEFOLDER $TARGETFOLDER
 bye
 "
